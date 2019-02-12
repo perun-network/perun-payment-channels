dd979c027d30849810570ffebefec5cc293b2f4d
Sebastian Stammler <stammler@cs.tu-darmstadt.de>
2019-02-12 15:38:35 +0100

    :white_check_mark: :art: add state assertions, reorganize tests

--- src/test/ledgerchannels.ts
+++ src/test/ledgerchannels.ts
@@ -14,66 +14,14 @@ contract("LedgerChannels", async (accounts) => {
   let idSeed = toBN(0xdeadbeef);
   let id: BN;
   let balance = {A: ether(10), B: ether(20)};
+  let chanUpdates = new Map<string, ChannelUpdate>();
   const timeout = 60;
 
-  describe("Opening...", () => {
-    it("should deploy the LedgerChannels contract", async () => {
-      lc = await LedgerChannels.deployed();
-    });
-
-    it("should generate the correct id from the seed", async () => {
-      id = await lc.genId(accounts[0], accounts[1], idSeed);
-      const expId = toBN(keccak(addr(accounts[0]), addr(accounts[1]), idSeed));
-      assert(id.eq(expId), "Generated id doesn't match spec.")
-    });
-
-    it("should open a ledger channel request from accounts[0] to [1]", async () => {
-      // open new channel with acc[1], timeout of 60 sec, 1 eth
-      let openTx = await lc.open(idSeed, timeout, accounts[1], balance.B, {value: balance.A});
-      let eventOpening = openTx.logs[0];
-
-      eventOpening.event.should.equal("Opening", "open() didn't fire an Opening event.");
-      assert(eventOpening.args.id.eq(id), "Opening id doesn't match.");
-      assert(eventOpening.args.balanceA.eq(balance.A), "Opening balance doesn't match.");
-      eventOpening.args.should.include(
-        {initiator: accounts[0], confirmer: accounts[1]},
-        "Opening event parties don't match");
-    });
-  })
-
-  snapshot("Opening; close timed-out opening", () => {
-    it("should not let the initiator close the channel", async () => {
-      return truffleAssert.reverts(lc.timeoutOpen(id),
-        "Confirmation timeout not reached yet.");
-    });
-
-    it("should not let accounts[2] confirm the channel", async () => {
-      return truffleAssert.reverts(
-        lc.confirmOpen(id, {value: balance.B, from: accounts[2]}),
-        "Caller is not the confirmer of this channel.");
-    });
-
-    it("should advance the blocktime by ~70 seconds", async () => {
-      let timestamp0 = await currentTimestamp();
-      await advanceBlockTime(timeout + 10)
-      let timestamp1 = await currentTimestamp();
-      // allow for up to 10 seconds to pass in the environment
-      expect(timestamp1 - timestamp0).to.be.within(timeout + 10, timeout + 20);
-    });
-
-    it("should let the initiator close the timed-out channel", async () => {
-      let timeoutOpenTx = await lc.timeoutOpen(id);
-      truffleAssert.eventEmitted(timeoutOpenTx, 'OpenTimeout', (ev: any) => {
-        return ev.id.eq(id) && ev.initiator === accounts[0]
-          && ev.confirmer === accounts[1] && ev.balanceA.eq(balance.A);
-      });
-      truffleAssert.eventEmitted(timeoutOpenTx, 'Withdrawal', (ev: any) => {
-        return ev.id.eq(id) && ev.by === accounts[0] && ev.balance.eq(balance.A);
-      });
-    });
-  }) /* snapshot Opening */
-
-  let chanUpdates = new Map<string, ChannelUpdate>();
+  async function assertState(id: BN, state: BN) {
+    let c = await lc.getChannel(id);
+    // Watch https://github.com/ethereum-ts/TypeChain/issues/139
+    assert(state.eq(toBN(c.state as unknown as string)), "Channel in wrong state.");
+  }
 
   async function assertCloseFail(closeFn: any, chanUpdateId: string, msg?: string) {
     let u = chanUpdates.get(chanUpdateId) as ChannelUpdate;
@@ -114,12 +62,71 @@ contract("LedgerChannels", async (accounts) => {
     });
   }
 
+  describe("Opening...", () => {
+    it("should deploy the LedgerChannels contract", async () => {
+      lc = await LedgerChannels.deployed();
+    });
+
+    it("should generate the correct id from the seed", async () => {
+      id = await lc.genId(accounts[0], accounts[1], idSeed);
+      const expId = toBN(keccak(addr(accounts[0]), addr(accounts[1]), idSeed));
+      assert(id.eq(expId), "Generated id doesn't match spec.")
+    });
+
+    it("channel should be in Null state", async () => {
+      return assertState(id, State.Null);
+    });
+
+    it("should open a ledger channel request from accounts[0] to [1]", async () => {
+      // open new channel with acc[1], timeout of 60 sec, 1 eth
+      let openTx = await lc.open(idSeed, timeout, accounts[1], balance.B, {value: balance.A});
+      let eventOpening = openTx.logs[0];
+
+      eventOpening.event.should.equal("Opening", "open() didn't fire an Opening event.");
+      assert(eventOpening.args.id.eq(id), "Opening id doesn't match.");
+      assert(eventOpening.args.balanceA.eq(balance.A), "Opening balance doesn't match.");
+      eventOpening.args.should.include(
+        {initiator: accounts[0], confirmer: accounts[1]},
+        "Opening event parties don't match");
+      return assertState(id, State.Opening);
+    });
+  })
+
+  snapshot("Opening; close timed-out opening", () => {
+    it("should not let the initiator close the channel", async () => {
+      return truffleAssert.reverts(lc.timeoutOpen(id),
+        "Confirmation timeout not reached yet.");
+    });
+
+    it("should not let accounts[2] confirm the channel", async () => {
+      return truffleAssert.reverts(
+        lc.confirmOpen(id, {value: balance.B, from: accounts[2]}),
+        "Caller is not the confirmer of this channel.");
+    });
+
+    it("should advance the blocktime by ~70 seconds", async () => {
+      let timestamp0 = await currentTimestamp();
+      await advanceBlockTime(timeout + 10)
+      let timestamp1 = await currentTimestamp();
+      // allow for up to 10 seconds to pass in the environment
+      expect(timestamp1 - timestamp0).to.be.within(timeout + 10, timeout + 20);
+    });
+
+    it("should let the initiator close the timed-out channel", async () => {
+      let timeoutOpenTx = await lc.timeoutOpen(id);
+      truffleAssert.eventEmitted(timeoutOpenTx, 'OpenTimeout', (ev: any) => {
+        return ev.id.eq(id) && ev.initiator === accounts[0]
+          && ev.confirmer === accounts[1] && ev.balanceA.eq(balance.A);
+      });
+      truffleAssert.eventEmitted(timeoutOpenTx, 'Withdrawal', (ev: any) => {
+        return ev.id.eq(id) && ev.by === accounts[0] && ev.balance.eq(balance.A);
+      });
+      return assertState(id, State.Null);
+    });
+  }) /* snapshot Opening */
+
   describe("Open...", () => {
     it("should confirm a channel between accounts[0] and [1]", async () => {
-      //truffleAssert.eventEmitted(
-        //await lc.open(idSeed, timeout, accounts[1], balance.B, {value: balance.A}),
-        //'Opening'
-      //);
       truffleAssert.eventEmitted(
         await lc.confirmOpen(id, {value: balance.B, from: accounts[1]}), 'Open',
         (ev: any) => { return ev.id.eq(id)
@@ -127,6 +134,7 @@ contract("LedgerChannels", async (accounts) => {
             && ev.balanceA.eq(balance.A) && ev.balanceB.eq(balance.B);
         }
       );
+      return assertState(id, State.Open);
     });
 
     it("generating state updates...", async () => {
@@ -158,18 +166,21 @@ contract("LedgerChannels", async (accounts) => {
 
   snapshot("Open; initial balance closing", () => {
     it("should request initial balance closing", async () => {
-      return assertCloseEvent(lc.closeInitialBalance, false, "Closing",
+      await assertCloseEvent(lc.closeInitialBalance, false, "Closing",
         accounts[0], accounts[0], accounts[1], '0');
+      return assertState(id, State.ClosingByA);
     });
 
     it("should have [1] confirm initial balance closing", async () => {
       let confirmCloseTx = await assertCloseEvent(lc.confirmClose, false, "Closed",
         accounts[1], accounts[0], accounts[1], '0');
       assertHasWithdrawalEvent(confirmCloseTx, id, accounts[1], balance.B);
+      return assertState(id, State.Closed);
     });
 
     it("should have [0] withdraw their channel balance", async () => {
       assertHasWithdrawalEvent(await lc.withdraw(id), id, accounts[0], balance.A);
+      return assertState(id, State.Null);
     });
   });
 
@@ -181,7 +192,8 @@ contract("LedgerChannels", async (accounts) => {
     });
 
     it("should request channel closing by [0] with version 1", async () => {
-      return assertCloseEvent(lc.close, true, "Closing", accounts[0], accounts[0], accounts[1], '1');
+      await assertCloseEvent(lc.close, true, "Closing", accounts[0], accounts[0], accounts[1], '1');
+      return assertState(id, State.ClosingByA);
     });
   });
 
@@ -204,11 +216,13 @@ contract("LedgerChannels", async (accounts) => {
       let tx = await assertCloseEvent(lc.timeoutClose, false, "ClosedTimeout",
         accounts[0], accounts[0], accounts[1], '1');
       assertHasWithdrawalEvent(tx, u.id, accounts[0], u.balanceA);
+      return assertState(id, State.Closed);
     });
 
     it("should let the confirmer withdraw their balance nonetheless", async () => {
       assertHasWithdrawalEvent(await lc.withdraw(u.id, {from: accounts[1]}),
         u.id, accounts[1], u.balanceB);
+      return assertState(id, State.Null);
     });
   });
 
@@ -223,10 +237,12 @@ contract("LedgerChannels", async (accounts) => {
       let tx = await assertCloseEvent(lc.disputedClose, true, "ClosedDisputed",
         accounts[1], accounts[0], accounts[1], '2');
       assertHasWithdrawalEvent(tx, u.id, accounts[1], u.balanceB);
+      return assertState(id, State.Closed);
     });
 
     it("should let [0] withdraw their balance nonetheless", async () => {
       assertHasWithdrawalEvent(await lc.withdraw(u.id), u.id, accounts[0], u.balanceA);
+      return assertState(id, State.Null);
     });
   });
 });
