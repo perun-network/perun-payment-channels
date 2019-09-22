d7f4e32239c00cc93e765c9c477fe6f7ab0d13e0
Sebastian Stammler <stammler@cs.tu-darmstadt.de>
2019-09-22 20:16:07 +0200

    :sparkles: [client] implemented channel opening protocol

--- client/client.js
+++ client/client.js
@@ -1,10 +1,31 @@
 const http = require('http');
 const ethers = require('ethers')
+const utils = ethers.utils;
 const chalk = require('chalk');
 const WebSocketServer = require('websocket').server;
 const WebSocketClient = require('websocket').client;
 
+const Channel = require('./channel').Channel;
 const PROTOCOL= 'perun-payment-channel';
+
+// contract session - has to be injected with setup()
+let contract = null;
+let wallet = null; // inject with setup()
+let name = '';
+
+// conns - map from peer names to connections
+let conns = {};
+// props - map from peer names to proposals
+let props = {};
+// chans - map from ids to channels
+let chans = {};
+
+// client dependency injector
+function setup(_contract, _wallet, _name) {
+  contract = _contract;
+  wallet = _wallet;
+  name = _name;
+}
 
 function listen(host, port) {
   let server = http.createServer(function(request, response) {
@@ -25,13 +46,15 @@ function listen(host, port) {
     let conn = request.accept(PROTOCOL, request.origin);
     console.log((new Date()) + ' Connection accepted from ' + request.origin);
 
+    conns[request.origin] = conn;
     setupConn(conn);
   });
 }
 
-// open opens a ws connection to the url specified in the proposal and
-// immediately sends the channel opening proposal.
-function open(proposal) {
+// connect opens a ws connection to the given url and saves it under the
+// specified peer identifier
+function connect(peer, url) {
+  // TODO: check if connection to peer already present.
   let client = new WebSocketClient();
 
   client.on('connectFailed', function(error) {
@@ -41,13 +64,31 @@ function open(proposal) {
   client.on('connect', function(conn) {
     console.log((new Date()) + " WebSocket Client connected.");
 
+    conns[peer] = conn;
     setupConn(conn);
-
-    // we immediately send the opening proposal as client
-    conn.sendUTF(JSON.stringify({ 'type': 'proposal', 'data': proposal }));
   })
 
-  client.connect(proposal.url, PROTOCOL, proposal.origin);
+  client.connect(url, PROTOCOL, name);
+}
+
+// proposeChannel sends a channel proposal on the specified connection
+async function proposeChannel(peer, proposal) {
+  console.log("Proposing\n" + JSON.stringify(proposal));
+  let conn = conns[peer];
+  if (!conn) {
+    console.log("I don't have a connection to peer: " + peer);
+    return;
+  }
+
+  let bal = await wallet.getBalance();
+  // Proposer always has index 0, accepter index 1
+  if (bal.lt(proposal.bals[0])) {
+    console.log("Insufficient funds for own proposal " + proposal);
+    return;
+  }
+
+  props[peer] = proposal;
+  conn.sendUTF(JSON.stringify({ 'type': 'proposal', 'data': proposal }));
 }
 
 // common connection handling for client and server
@@ -61,22 +102,24 @@ function setupConn(conn) {
       + '['+reasonCode+'] ' + description);
   });
 
-  conn.on('message', function(message) {
+  conn.on('message', async function(message) {
     if (message.type !== 'utf8') {
-      console.log(chalk.red("Received bogus binary message."));
+      warn("Received bogus binary message.");
       return;
     }
 
-    handleMsg(message.utf8Data, conn);
+    return handleMsg(conn, JSON.parse(message.utf8Data));
   });
 }
 
-function handleMsg(msg, conn) {
+async function handleMsg(conn, msg) {
+  console.log("Received message:");
+  console.log(msg);
   switch (msg.type) {
     case 'proposal':
-      break;
+      return handleProp(conn, msg.data);
     case 'accept':
-      break;
+      return handleAccept(conn, msg.data);
     case 'updateReq':
       break;
     case 'updateRes':
@@ -86,7 +129,87 @@ function handleMsg(msg, conn) {
   }
 }
 
+async function handleProp(conn, prop) {
+  ethifyProposal(prop);
+  // check that we have enough eth in our account
+  let bal = await wallet.getBalance();
+  // Proposer always has index 0, accepter index 1
+  if (bal.lt(prop.bals[1])) {
+    console.log("Insufficient funds for peer prop " + prop);
+    return
+  }
+  // complete proposal - our address is missing so far
+  prop.parts[1] = wallet.address;
+  // we accept all proposals in this demo...
+  conn.sendUTF(JSON.stringify({ 'type': 'accept', 'data': prop }));
+
+  // setup local channel instance
+  // We are accepter -> idx 1
+  let chan = Channel.fromProp(prop, 1, wallet);
+  chans[chan.id] = chan;
+
+  // wait for Opening event caused by proposer
+  // The channel id doesn't commit to the balances, so we filter that one too.
+  let eventOpening = contract.filters.Opening(chan.id);
+  contract.once(eventOpening, async () => {
+    // TODO: check that proposer sent the funds he promised...
+    console.log("[Opening] Proposer funded channel, funding...");
+    let tx = await contract.confirmOpen(
+      chan.id,
+      { value: chan.bals[1] }
+    );
+
+    console.log("confirmOpen called.");
+    await tx.wait();
+    console.log("confirmOpen tx mined. Channel is open! 🎉");
+  });
+}
+
+async function handleAccept(conn, prop) {
+  // TODO: check that proposal matches the one we sent!
+  ethifyProposal(prop);
+
+  // setup local channel instance
+  // We are proposer -> idx 0
+  let chan = Channel.fromProp(prop, 0, wallet);
+  chans[chan.id] = chan;
+
+  // Proposer (we) receives accept -> counterParty has idx 1
+  let tx = await contract.open(
+    chan.nonce,
+    chan.timeoutDur,
+    chan.parts[1],
+    chan.bals[1],
+    { value: chan.bals[0] }
+  )
+  console.log("open called.");
+  //console.log(tx);
+
+  // wait for tx to be mined.
+  await tx.wait();
+  console.log("open tx mined. Wait for Open event caused by funding from peer.");
+
+  let eventOpen = contract.filters.Open(chan.id);
+  contract.once(eventOpen, () => {
+    console.log("[Open] Peer funded channel, it is open! 🎉")
+    // TODO: set state of channel to funded - currently we don't track state
+  });
+}
+
+// JSON unmarshaler for proposals send over the wire
+function ethifyProposal(proposal) {
+  proposal.nonce = utils.bigNumberify(proposal.nonce);
+  proposal.bals = proposal.bals.map(x => utils.bigNumberify(x));
+  proposal.parts = proposal.parts.map(x => x ? utils.getAddress(x) : null);
+}
+
+function warn(msg) {
+  console.error(chalk.red(msg));
+}
+
 module.exports = {
+  setup: setup,
   listen: listen,
-  open: open
+  connect: connect,
+  proposeChannel: proposeChannel
 }
